61e4143c8f7cc570ae96103555c5b5c263727223
Luca Patrignani <luca.patrignani3@studio.unibo.it>
2025-04-14 23:52:05 +0200

    Remove unused imports and rename stuff

--- src/main/java/com/example/FutureDependencyAnalyzer.java
+++ src/main/java/com/example/FutureDependencyAnalyzer.java
@@ -3,8 +3,6 @@ package com.example;
 import com.github.javaparser.StaticJavaParser;
 import com.github.javaparser.ast.CompilationUnit;
 import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
-import com.github.javaparser.ast.body.TypeDeclaration;
-import com.github.javaparser.ast.expr.ClassExpr;
 import com.github.javaparser.ast.type.ClassOrInterfaceType;
 import com.github.javaparser.symbolsolver.JavaSymbolSolver;
 import com.github.javaparser.symbolsolver.resolution.typesolvers.CombinedTypeSolver;
@@ -12,7 +10,6 @@ import com.github.javaparser.symbolsolver.resolution.typesolvers.ReflectionTypeS
 import io.vertx.core.Future;
 
 import java.io.InputStream;
-import java.util.HashSet;
 import java.util.Set;
 import java.util.stream.Collectors;
 
@@ -26,26 +23,28 @@ public class FutureDependencyAnalyzer {
                     StaticJavaParser.getParserConfiguration().setSymbolResolver(symbolSolver);
                     return Future.succeededFuture(StaticJavaParser.parse(code));
                 });
-        Future<Set<String>> usedTypes = compilationUnit.compose(
+        Future<Set<String>> usedTypesFuture = compilationUnit.compose(
                 cu -> Future.succeededFuture(
                         cu.findAll(ClassOrInterfaceType.class).stream()
                                 .map(ClassOrInterfaceType::getNameAsString)
                                 .collect(Collectors.toSet())
                 )
         );
-        Future<Set<String>> declaredTypes = compilationUnit.compose(
+        Future<Set<String>> declaredTypesFuture = compilationUnit.compose(
                 cu -> Future.succeededFuture(
                         cu.findAll(ClassOrInterfaceDeclaration.class).stream()
                                 .map(ClassOrInterfaceDeclaration::getNameAsString)
                                 .collect(Collectors.toSet())
                 )
         );
-        return Future.all(usedTypes, declaredTypes)
+        return Future.all(usedTypesFuture, declaredTypesFuture)
                 .compose(compositeFuture -> {
-                    final var s1 = (Set<String>)compositeFuture.resultAt(0);
-                    final var s2 = (Set<String>)compositeFuture.resultAt(1);
-                    s1.removeAll(s2);
-                    return Future.succeededFuture(s1);
+                    @SuppressWarnings("unchecked")
+                    final var usedTypes = (Set<String>)compositeFuture.resultAt(0);
+                    @SuppressWarnings("unchecked")
+                    final var declaredTypes = (Set<String>)compositeFuture.resultAt(1);
+                    usedTypes.removeAll(declaredTypes);
+                    return Future.succeededFuture(usedTypes);
                 });
     }
 
